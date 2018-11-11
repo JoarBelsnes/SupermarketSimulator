c48b2255e4334370a9c3434d820f9905f606bb80
JSachar <43283342+jsachar@users.noreply.github.com>
2018-11-11 15:50:54 -0500

    Gui Update
    Added Menu Bar, moveed to VBox

--- src/Gui.java
+++ src/Gui.java
@@ -3,7 +3,14 @@ import javafx.application.Application;
 import javafx.collections.*;
 import javafx.scene.Scene;
 import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button; 
+import javafx.scene.layout.GridPane; 
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text; 
+import javafx.scene.control.TextField; 
 import javafx.stage.Stage;
 
 public class Gui extends Application {
@@ -13,19 +20,34 @@ public class Gui extends Application {
 
     public void start(Stage primaryStage) {
 
+        // Menu Bar
+        MenuBar menuBar = new MenuBar();
+        // Menu - File
+        Menu menuFile = new Menu("File");
+        MenuItem menuFileNew = new MenuItem("New");
+        MenuItem menuFileLoad = new MenuItem("Load");
+        MenuItem menuFileSave = new MenuItem("Save");
+        MenuItem menuFileSaveAs = new MenuItem("Save As");
+        MenuItem menuFileExit = new MenuItem("Exit");
+        menuFile.getItems().addAll(menuFileNew, menuFileLoad, menuFileSave, menuFileSaveAs, menuFileExit);
+        // Menu - Simulations
+        Menu menuSimulation = new Menu("Simulation");
+        MenuItem menuSimulationRun = new MenuItem("Run");
+        MenuItem menuSimulationHalt = new MenuItem("Halt");
+        menuSimulation.getItems().addAll(menuSimulationRun, menuSimulationHalt);
+        
+        menuBar.getMenus().addAll(menuFile, menuSimulation);
 
+        // VBox and Scene Initialization
+        VBox vBox = new VBox(menuBar);
+        Scene primaryScene = new Scene(vBox, 400, 200);
 
-    Group root = new Group();
-    ObservableList list = root.getChildren();
-    StackPane pane = new StackPane();
+        // Scene Settings
 
-
-
-
-
-    primaryStage.setTitle("Supermarket Simulator");
-    primaryStage.setScene(new Scene(root, 200, 200));
-    primaryStage.show();
+        // Stage Settings
+        primaryStage.setTitle("Supermarket Simulator");
+        primaryStage.setScene(primaryScene);
+        primaryStage.show();
 
 
     }
