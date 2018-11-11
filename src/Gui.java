3e542d7cf8d1cd475f6d0d164fd949e7ef2d8505
JSachar <43283342+jsachar@users.noreply.github.com>
2018-11-11 17:00:03 -0500

    GUI Update 11/11 #2
    Added some fields and labels, a combo box for selecting queue selection. Has all the fields from the documentation, Do we need more?
    
    Also, need to think of a good CSS to make it look a little nicer.

--- src/Gui.java
+++ src/Gui.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button; 
 import javafx.scene.layout.GridPane; 
 import javafx.scene.layout.StackPane;
@@ -35,15 +36,51 @@ public class Gui extends Application {
         MenuItem menuSimulationRun = new MenuItem("Run");
         MenuItem menuSimulationHalt = new MenuItem("Halt");
         menuSimulation.getItems().addAll(menuSimulationRun, menuSimulationHalt);
-        
+
         menuBar.getMenus().addAll(menuFile, menuSimulation);
 
-        // VBox and Scene Initialization
-        VBox vBox = new VBox(menuBar);
-        Scene primaryScene = new Scene(vBox, 400, 200);
+        // Creating Labels
+        Text lblMaxCustomers = new Text("Max. Customers: ");
+        Text lblNumberOfCheckoutLines = new Text("# of Checkout Lines: ");
+        Text lblNumberOfLimitedCheckoutLines = new Text("# of 'x Items or Less' Checkout Lines: ");
+        Text lblMaxSimulationTime = new Text("Max. Simulation Time: ");
+        Text lblQueueSelectionMethod = new Text("Queue Selection Method");
+
+
+        // Creating Text Fields
+        TextField txtMaxCustomers = new TextField();
+        TextField txtNumberOfCheckoutLines = new TextField();
+        TextField txtNumberOfLimitedCheckoutLines = new TextField();
+        TextField txtMaxSimulationTime = new TextField();
+
+        // Combo Boxes
+        ComboBox cmbQueueSelectionMethod = new ComboBox();
+        cmbQueueSelectionMethod.getItems().addAll(
+            "Store Assigned",
+            "Customer Selected",
+            "Random Assignment"
+            );
+
+        // Grid Pane
+        GridPane grid = new GridPane();
+        grid.setVgap(5);
+        grid.setHgap(25);
+        grid.setPadding(new Insets(5, 5, 5, 5));
+        grid.add(lblMaxCustomers, 0, 0);
+        grid.add(txtMaxCustomers, 1, 0);
+        grid.add(lblQueueSelectionMethod, 2, 0);
+        grid.add(cmbQueueSelectionMethod, 2, 1, 2, 1);
+        grid.add(lblNumberOfCheckoutLines, 0, 1);
+        grid.add(txtNumberOfCheckoutLines, 1, 1);
+        grid.add(lblNumberOfLimitedCheckoutLines, 0, 2);
+        grid.add(txtNumberOfLimitedCheckoutLines, 1, 2);
+        grid.add(lblMaxSimulationTime, 0, 3);
+        grid.add(txtMaxSimulationTime, 1, 3);
 
         // Scene Settings
-
+        VBox root = new VBox();
+        root.getChildren().addAll(menuBar, grid);
+        Scene primaryScene = new Scene(root, 600, 300);
         // Stage Settings
         primaryStage.setTitle("Supermarket Simulator");
         primaryStage.setScene(primaryScene);
