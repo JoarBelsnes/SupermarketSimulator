ed494e3f7ce13f65169417a721c25b54f5cc6394
liambeliveau <liambeliveau10001@gmail.com>
2018-11-15 20:29:06 -0500

    moved all of main to an instantiable supermarket class

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,7 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-
 public class Main {
 
     public static void main(String[] args) {
-        int maxTime = 1000;
-        int numCustomers = 20;
-        Random rand = new Random();
-
-        //create customers here
-        ArrayList<Customer> customers = generateCustomers(numCustomers);
-
-        //create priority queue for events, ordered by start time
-        PriorityQueue<Event> eventQueue = new PriorityQueue<>((e1, e2) -> {
-            if (e1.getStartTime() > e2.getStartTime()) {
-                return 1;
-            }
-            if (e1.getStartTime() < e2.getStartTime()) {
-                return -1;
-            }
-            return 0;
-        });
-
-        //add customer arrivals to event queue
-        for (Customer customer : customers) {
-            eventQueue.add(new Event(customer, type.CUSTOMER_ARRIVES, rand.nextInt(101)));
-        }
-
-        /*
-        //poll the entire queue for testing purposes; remove this when actually using it
-        for(int i = 0; i < numCustomers; i++){
-            System.out.println("Customer arriving at " + eventQueue.poll().getStartTime());
-        }
-        */
-
-        /************ CREATING TIME ***************/
-        //testing to see if setArrival actually works
-        int currentTime = 0;
-
-        while (currentTime < maxTime) {
-            try {
-                //handles event here
-
-                //while firstEventInQueue is at currentTime
-                //  handle firstEventInQueue
-                //  remove firstEventInQueue
-
-                TimeUnit.SECONDS.sleep(1);
-                currentTime++;
-            } catch (InterruptedException e) {
-
-
-            }
-            System.out.println("Current System Time: " + currentTime);
-        }
-    }
-
-    //generates an arraylist of customers of a specified size, with random numbers of items
-    private static ArrayList<Customer> generateCustomers(int numCustomers) {
-        Random rand = new Random();
-        ArrayList<Customer> customers = new ArrayList<>(numCustomers);
-        for (int i = 0; i < numCustomers; i++) {
-            customers.add(new Customer(Integer.toString(i), rand.nextInt(51)));
-        }
-        return customers;
+        Supermarket supermarket = new Supermarket(20,10,2);
+        supermarket.run();
     }
 }
---
+++ src/Supermarket.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Supermarket {
+    private int maxSimulationTime;
+    private int numCustomers;
+    private int numCashiers;
+    private PriorityQueue<Event> eventQueue;
+    private ArrayList<Customer> customers;
+    private ArrayList<Cashier> cashiers;
+    private int currentTime = 0;
+    private Random rand = new Random();
+
+    //constructor
+    public Supermarket(int maxSimulationTime, int numCustomers, int numCashiers) {
+        this.maxSimulationTime = maxSimulationTime;
+        this.numCustomers = numCustomers;
+        this.numCashiers = numCashiers;
+        customers = generateCustomers(numCustomers);
+        cashiers = new ArrayList<Cashier>(numCashiers);
+
+        //event queue is priority sorted by lowest time
+        eventQueue = new PriorityQueue<>((e1, e2) -> {
+            if (e1.getStartTime() > e2.getStartTime()) {
+                return 1;
+            }
+            if (e1.getStartTime() < e2.getStartTime()) {
+                return -1;
+            }
+            return 0;
+        });
+
+        //add customer arrivals to event queue
+        for (Customer customer : customers) {
+            eventQueue.add(new Event(customer, type.CUSTOMER_ARRIVES, rand.nextInt(maxSimulationTime + 1)));
+        }
+
+    }
+
+    /***
+     * generates an arraylist of customers of a specified size, with random size shopping lists
+     * @param numCustomers number of customers to enter the store
+     * @return arraylist of all customers
+     */
+    private ArrayList<Customer> generateCustomers(int numCustomers) {
+        Random rand = new Random();
+        ArrayList<Customer> customers = new ArrayList<>(numCustomers);
+        for (int i = 0; i < numCustomers; i++) {
+            //items range from 1-50
+            customers.add(new Customer(Integer.toString(i), rand.nextInt(51)));
+        }
+        return customers;
+    }
+
+    public void run() {
+        currentTime = 0;
+        Event currentEvent;
+
+        while (currentTime < maxSimulationTime) {
+            try {
+                //handles event here
+                while (eventQueue.size() > 0 && eventQueue.peek().getStartTime() == currentTime) {
+                    //poll returns the event while removing it from the queue, use that to handle it
+                    currentEvent = eventQueue.poll();
+
+                    //placeholder for actually handling an event
+                    switch (currentEvent.getEventType()) {
+                        case CUSTOMER_ARRIVES:
+                            System.out.println("A customer arrived");
+                            break;
+                        case CUSTOMER_READY_CHECKOUT:
+                            System.out.println("A customer is ready for checkout");
+                            break;
+                        case CUSTOMER_FINISH_CHECKOUT:
+                            System.out.println("A customer finished checkout");
+                            break;
+                        case CUSTOMER_CHANGE_LINE:
+                            System.out.println("A customer wants to change lines");
+                            break;
+                        case CUSTOMER_ABANDON:
+                            System.out.println("A customer abandoned the store");
+                            break;
+                    }
+                }
+
+                TimeUnit.SECONDS.sleep(1);
+                currentTime++;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("Current System Time: " + currentTime);
+        }
+    }
+
+}
