cc8a210c5c4635ac6d4fffe99746833a2b1c1844
Andrew Thomas McNulty <andrew.mcnulty@uvm.edu>
2018-12-02 17:12:57 -0500

    created a new thread to handle the gui, using Progress.runLater() to update javafx

--- src/Gui.java
+++ src/Gui.java
@@ -4,7 +4,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.awt.Desktop;
 import java.io.*;
+
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -35,8 +38,6 @@ import java.util.ArrayList;
 public class Gui extends Application {
 
 
-
-
     public void start(Stage primaryStage) {
 
 
@@ -47,9 +48,9 @@ public class Gui extends Application {
         FileChooser fileSelector = new FileChooser();
         fileSelector.setInitialDirectory(new File(System.getProperty("user.home")));
         fileSelector.getExtensionFilters().addAll(
-            new FileChooser.ExtensionFilter("Supermaket Settings", "*.mkt"),
-            new FileChooser.ExtensionFilter("Text Document", "*.txt")
-            );
+                new FileChooser.ExtensionFilter("Supermaket Settings", "*.mkt"),
+                new FileChooser.ExtensionFilter("Text Document", "*.txt")
+        );
 
         // Menu Bar
         MenuBar menuBar = new MenuBar();
@@ -81,7 +82,6 @@ public class Gui extends Application {
         Button runButton = new Button("Run Simulation");
 
 
-
         // Grid Pane
         GridPane grid = new GridPane();
         grid.setVgap(5);
@@ -95,28 +95,15 @@ public class Gui extends Application {
         // grid.add(txtNumberOfLimitedCheckoutLines, 1, 2);
         grid.add(lblMaxSimulationTime, 0, 3);
         grid.add(txtMaxSimulationTime, 1, 3);
-        grid.add(runButton,1,4);
+        grid.add(runButton, 1, 4);
         // Scene Settings
         VBox root = new VBox();
         root.getChildren().addAll(menuBar, grid);
         Scene primaryScene = new Scene(root, 600, 300);
 
 
-
-
-
         //simulation scene
         //array of cashiers
-        ArrayList<Text> cashiers = new ArrayList<Text>();
-        int numberOfLines = 4;
-        //array of customers
-        ArrayList<Text> Customers = new ArrayList<Text>();
-        ArrayList<Integer> numberOfCustomers = new ArrayList<Integer>();
-
-        numberOfCustomers.add(4);
-        numberOfCustomers.add(6);
-        numberOfCustomers.add(5);
-        numberOfCustomers.add(7);
 
 
         GridPane simulationPane = new GridPane();
@@ -125,8 +112,8 @@ public class Gui extends Application {
         simulationPane.setPadding(new Insets(5, 5, 5, 5));
         Button runButton2 = new Button("Run Simulation");
         Button back = new Button("Back");
-        simulationPane.add(back, 5,1);
-        simulationPane.add( runButton2, 1,1);
+        simulationPane.add(back, 5, 1);
+        simulationPane.add(runButton2, 1, 1);
 
 
         Group root2 = new Group();
@@ -134,26 +121,23 @@ public class Gui extends Application {
 
 
         Scene simulationScene = new Scene(root2, 800, 400);
+        /**
+         //for loop to generate cashiers
+         for (int i = 0; i < numberOfLines; i++ ){
+         Text t = new Text("Line " + (i+1));
+         cashiers.add(t);
+         simulationPane.add(cashiers.get(i), (i+1)*2, 3);
+         }
+         for (int x = 0; x < numberOfCustomers.size(); x++){
 
-        //for loop to generate cashiers
-        for (int i = 0; i < numberOfLines; i++ ){
-            Text t = new Text("Line " + (i+1));
-            cashiers.add(t);
-            simulationPane.add(cashiers.get(i), (i+1)*2, 3);
-        }
-        for (int x = 0; x < numberOfCustomers.size(); x++){
+         for (int y = 0; y < numberOfCustomers.get(x)-1; y++){
+         Text c = new Text("Customer "+ (y+1));
+         Customers.add(c);
 
-            for (int y = 0; y < numberOfCustomers.get(x)-1; y++){
-                Text c = new Text("Customer "+ (y+1));
-                Customers.add(c);
-
-                simulationPane.add(c,(x+1)*2,y+4);
-            }
-        }
-
-
-
-
+         simulationPane.add(c,(x+1)*2,y+4);
+         }
+         }
+         **/
 
 
         // Stage Settings
@@ -162,44 +146,44 @@ public class Gui extends Application {
         primaryStage.show();
 
         // Handlers
-        menuFileExit.setOnAction(new EventHandler<ActionEvent>(){
+        menuFileExit.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent t) {
                 System.exit(0);
             }
         });
-        menuFileNew.setOnAction(new EventHandler<ActionEvent>(){
+        menuFileNew.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent t) {
                 txtMaxCustomers.setText("10");
                 txtMaxSimulationTime.setText("50");
                 txtNumberOfCheckoutLines.setText("2");
             }
         });
-        menuFileLoad.setOnAction(new EventHandler<ActionEvent>(){
+        menuFileLoad.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent t) {
                 fileSelector.setTitle("Load Settings");
                 File file = fileSelector.showOpenDialog(primaryStage);
-                if (file != null){
+                if (file != null) {
                     BufferedReader br = null;
-                    try { 
-                            br = new BufferedReader(new FileReader(file));
-                            String line = br.readLine();
-                            String[] supermarketSettings = line.split(",");
-                            txtMaxSimulationTime.setText(supermarketSettings[0]);
-                            txtMaxCustomers.setText(supermarketSettings[1]);
-                            txtNumberOfCheckoutLines.setText(supermarketSettings[2]);
+                    try {
+                        br = new BufferedReader(new FileReader(file));
+                        String line = br.readLine();
+                        String[] supermarketSettings = line.split(",");
+                        txtMaxSimulationTime.setText(supermarketSettings[0]);
+                        txtMaxCustomers.setText(supermarketSettings[1]);
+                        txtNumberOfCheckoutLines.setText(supermarketSettings[2]);
                     } catch (FileNotFoundException ex) {
                         ex.printStackTrace();
                     } catch (IOException ex) {
                         ex.printStackTrace();
-                    } catch (ArrayIndexOutOfBoundsException ex){
+                    } catch (ArrayIndexOutOfBoundsException ex) {
                         Alert alert = new Alert(AlertType.ERROR, "The file you selected is not in the expected format.", ButtonType.OK);
                         alert.showAndWait();
                     } finally {
-                        if (br != null){
+                        if (br != null) {
                             try {
                                 br.close();
-                            } catch (IOException ex){
+                            } catch (IOException ex) {
                                 ex.printStackTrace();
                             }
                         }
@@ -208,51 +192,59 @@ public class Gui extends Application {
                 }
             }
         });
-        menuFileSaveAs.setOnAction(new EventHandler<ActionEvent>(){
+        menuFileSaveAs.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent t) {
                 fileSelector.setTitle("Save Settings");
                 File file = fileSelector.showSaveDialog(primaryStage);
-                if (file != null){
+                if (file != null) {
                     try {
                         FileWriter fileWriter = new FileWriter(file);
                         fileWriter.write(txtMaxSimulationTime.getText() + "," + txtMaxCustomers.getText() + "," + txtNumberOfCheckoutLines.getText());
                         fileWriter.close();
                     } catch (IOException ex) {
-                        System.out.println (ex.getMessage());
+                        System.out.println(ex.getMessage());
                     }
 
                 }
             }
         });
-        runButton.setOnAction(new EventHandler<ActionEvent>(){
-            public void handle(ActionEvent t){
+        runButton.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
                 runButton2.setDisable(false);
                 primaryStage.setScene(simulationScene);
 
             }
         });
-        runButton2.setOnAction(new EventHandler<ActionEvent>(){
+        runButton2.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent t) {
                 runButton2.setDisable(true);
                 try {
                     Supermarket smart = new Supermarket(Integer.parseInt(txtMaxSimulationTime.getText()), Integer.parseInt(txtMaxCustomers.getText()), Integer.parseInt(txtNumberOfCheckoutLines.getText()));
                     Thread martThread = new Thread(smart::call);
                     martThread.start();
-                    back.setOnAction(new EventHandler<ActionEvent>(){
+
+                    runSimulation sim = new runSimulation(smart, simulationScene, simulationPane);
+                    Thread simThread = new Thread(sim::run);
+                    simThread.start();
+
+
+                    back.setOnAction(new EventHandler<ActionEvent>() {
                         public void handle(ActionEvent t) {
                             martThread.stop();
+                            simThread.stop();
                             primaryStage.setScene(primaryScene);
-                    }}); 
+                        }
+                    });
                 } catch (NumberFormatException ex) {
                     Alert alert = new Alert(AlertType.ERROR, "Your settings are incompatable. \nMake sure none of the fields are blank.", ButtonType.OK);
                     alert.showAndWait();
                 }
-            
+
             }
 
         });
-        back.setOnAction(new EventHandler<ActionEvent>(){
+        back.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent t) {
                 primaryStage.setScene(primaryScene);
             }
@@ -262,25 +254,25 @@ public class Gui extends Application {
 
         // Listeners
         // These Enforce only Integers in the text boxes
-        txtMaxCustomers.textProperty().addListener(new ChangeListener<String>(){
+        txtMaxCustomers.textProperty().addListener(new ChangeListener<String>() {
             @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue){
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
                 if (!newValue.matches("\\d*")) {
                     txtMaxCustomers.setText(newValue.replaceAll("[^\\d]", ""));
                 }
             }
         });
-        txtNumberOfCheckoutLines.textProperty().addListener(new ChangeListener<String>(){
+        txtNumberOfCheckoutLines.textProperty().addListener(new ChangeListener<String>() {
             @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue){
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
                 if (!newValue.matches("\\d*")) {
                     txtNumberOfCheckoutLines.setText(newValue.replaceAll("[^\\d]", ""));
                 }
             }
         });
-        txtMaxSimulationTime.textProperty().addListener(new ChangeListener<String>(){
+        txtMaxSimulationTime.textProperty().addListener(new ChangeListener<String>() {
             @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue){
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
                 if (!newValue.matches("\\d*")) {
                     txtMaxSimulationTime.setText(newValue.replaceAll("[^\\d]", ""));
                 }
@@ -292,4 +284,85 @@ public class Gui extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-}
+
+
+    public class runSimulation implements Runnable {
+        Supermarket s;
+        Scene x;
+        GridPane g;
+        ArrayList<Cashier> cashiers = new ArrayList<Cashier>();
+        ArrayList<Text> cashiersLabel = new ArrayList<Text>();
+        ArrayList<Text> Customers = new ArrayList<Text>();
+
+        runSimulation(Supermarket s, Scene x, GridPane g) {
+            this.s = s;
+            this.x = x;
+            this.g = g;
+        }
+
+        public void run() {
+            Runnable updater = new Runnable() {
+
+                public void run() {
+                    try{
+
+                            cashiers = s.getCashiers();
+                            //generates the cashiers
+                            for (int i = 0; i < cashiers.size(); i++) {
+                                Text t = new Text("Line Length: " + cashiers.get(i).getLineLength());
+                                cashiersLabel.add(t);
+                                if (!g.getChildren().contains(cashiersLabel.get(i))) {
+                                    g.add(cashiersLabel.get(i), (i), 3);
+                                }
+                                //generate customer line
+                            }
+
+
+                            cashiers = s.getCashiers();
+
+
+                            //generate the customer objects
+                            for(int x = 0; x < s.getCustomers().size(); x++){
+                                Text t = new Text("Customer" +s.getCustomers().get(x));
+                                Customers.add(t);
+
+                            }
+                            //puts the customers into line
+
+                                for (int z = 0; z < cashiers.size(); z++) {
+
+                                    for (int y = 0; y < cashiers.get(z).getLineLength(); y++) {
+                                        int c = cashiers.get(z).getCustomers().get(y);
+
+                                        g.add(Customers.get(c), (z), y + 4);
+
+                                    }
+                                }
+                                Thread.sleep(1000);
+                                g.getChildren().removeAll(Customers);
+                            
+
+                    } catch(InterruptedException e) {
+                        throw new RuntimeException(e);
+
+                    }
+                    }
+
+
+            };
+            Platform.runLater(updater);
+        }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
