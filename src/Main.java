8512b5c60089f1ede0bda3c8d2b0fbe7691eb8eb
liambeliveau <liambeliveau10001@gmail.com>
2018-12-04 18:35:22 -0500

    update unit tests, supermarket constructor now asks for mean arrival time

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         Thread martThread = new Thread(() -> {
-            Supermarket supermarket = new Supermarket(50, 30, 2);
+            Supermarket supermarket = new Supermarket(50, 30, 2, 10);
             supermarket.step();
             supermarket.step();
             supermarket.step();
--- src/SupermarketTest.java
+++ src/SupermarketTest.java
@@ -6,14 +6,14 @@ class SupermarketTest {
 
     @Test
     void step() {
-        Supermarket supermarketTest = new Supermarket(0,1,1);
+        Supermarket supermarketTest = new Supermarket(0,1,1,0);
         assertEquals(supermarketTest.step(), true);
         assertEquals(supermarketTest.getCurrentTime(), 1);
     }
 
     @Test
     void addEventToQueue() {
-        Supermarket supermarketTest = new Supermarket(0,0,0);
+        Supermarket supermarketTest = new Supermarket(0,0,0,0);
         assertEquals(supermarketTest.getEventQueue().poll(), null);
         supermarketTest.addEventToQueue(new Event(0,type.CUSTOMER_ARRIVES,0));
         assertEquals(supermarketTest.getEventQueue().poll().getEventType(), type.CUSTOMER_ARRIVES);
@@ -21,14 +21,14 @@ class SupermarketTest {
 
     @Test
     void handleEvent_arrival() {
-        Supermarket supermarketTest = new Supermarket(0, 1, 1);
+        Supermarket supermarketTest = new Supermarket(0, 1, 1,0);
         supermarketTest.step();
         assertEquals(supermarketTest.getEventQueue().poll().getEventType(), type.CUSTOMER_READY_CHECKOUT);
     }
 
     @Test
     void handleEvent_readyCheckout() {
-        Supermarket supermarketTest = new Supermarket(0, 1, 1);
+        Supermarket supermarketTest = new Supermarket(0, 1, 1,0);
         supermarketTest.addEventToQueue(new Event(0, type.CUSTOMER_READY_CHECKOUT, 0));
         supermarketTest.step();
         assertEquals(supermarketTest.getEventQueue().poll().getEventType(), type.CUSTOMER_FINISH_CHECKOUT);
@@ -36,14 +36,14 @@ class SupermarketTest {
 
     @Test
     void handleEvent_finishCheckout() {
-        Supermarket supermarketTest = new Supermarket(0, 0, 0);
+        Supermarket supermarketTest = new Supermarket(0, 0, 0,0);
         supermarketTest.addEventToQueue(new Event(0, type.CUSTOMER_FINISH_CHECKOUT, 0));
         assertEquals(supermarketTest.step(), false);
     }
 
     @Test
     void handleEvent_changeLine() {
-        Supermarket supermarketTest = new Supermarket(0, 1, 2);
+        Supermarket supermarketTest = new Supermarket(0, 1, 2,0);
         supermarketTest.addEventToQueue(new Event(0, type.CUSTOMER_READY_CHECKOUT, 1));
         supermarketTest.step();
         supermarketTest.addEventToQueue((new Event(0, type.CUSTOMER_CHANGE_LINE, 2)));
@@ -54,7 +54,7 @@ class SupermarketTest {
 
     @Test
     void handleEvent_abandon() {
-        Supermarket supermarketTest = new Supermarket(0, 0, 0);
+        Supermarket supermarketTest = new Supermarket(0, 0, 0,0);
         supermarketTest.addEventToQueue(new Event(0, type.CUSTOMER_ABANDON, 0));
         assertEquals(supermarketTest.step(), false);
     }
